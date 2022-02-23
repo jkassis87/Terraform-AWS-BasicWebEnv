e8251cd6979617f581e1b1edcc77a2adb85a6b5c
root <root@DESKTOP-4G77K3E.localdomain>
2022-02-23 21:43:05 +1100

    updated gitignore

--- variables.tf
+++ variables.tf
@@ -3,3 +3,20 @@
 variable "aws_region" {
   default = "ap-southeast-2"
 } 
+
+
+# --- database variables ---
+
+variable "dbname" {
+  type = string
+}
+
+variable "dbuser" {
+  type = string
+  sensitive = "true"
+}
+
+variable "dbpassword" {
+  type = string
+  sensitive = "true"
+}
